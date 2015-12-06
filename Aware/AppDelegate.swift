125fe5c783c2111c2360c9bbcc200e17e5499808
Joshua Peek <josh@joshpeek.com>
2015-12-05 22:10:13 -0800

    Initial Commit

---
+++ Aware/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Aware
+//
+//  Created by Joshua Peek on 12/5/15.
+//  Copyright © 2015 Joshua Peek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
