b0ff3d4e1d2fe78256ef07eb759530d165ab69ab
Joshua Peek <josh@joshpeek.com>
2015-12-05 22:43:32 -0800

    Start timer

--- Aware/AppDelegate.swift
+++ Aware/AppDelegate.swift
@@ -14,19 +14,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
 
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    var timerStart: NSDate?
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         if let button = statusItem.button {
-            button.attributedTitle = NSAttributedString(string: "1m")
+            button.attributedTitle = NSAttributedString(string: "0s")
         }
 
         let menu = NSMenu()
         menu.addItemWithTitle("Quit", action: "terminate:", keyEquivalent: "Q")
         statusItem.menu = menu
+
+        timerStart = NSDate()
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "onTick", userInfo: nil, repeats: true)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+
+    func onTick() {
+        let sinceStart = NSDate().timeIntervalSinceDate(timerStart!)
+        let secondsSinceStart = NSInteger(sinceStart) % 60
+        statusItem.button!.attributedTitle = NSAttributedString(string: "\(secondsSinceStart)s")
     }
 
 
