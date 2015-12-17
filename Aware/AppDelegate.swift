06d0e5d80706692f04d96463751b3cbada270ebd
Joshua Peek <josh@joshpeek.com>
2015-12-16 20:11:43 -0800

    Consolidate classes

--- Aware/AppDelegate.swift
+++ Aware/AppDelegate.swift
@@ -2,4 +2,36 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet weak var statusItem: StatusItem!
+
+    var timerStart: NSDate!
+
+    // Redraw button every minute
+    let buttonRefreshRate: NSTimeInterval = 60
+
+    // Reset timer after 2m of inactivity
+    let userInactivityTimeout: NSTimeInterval = 2 * 60
+
+    // kCGAnyInputEventType isn't part of CGEventType enum
+    // defined in <CoreGraphics/CGEventTypes.h>
+    let AnyInputEventType = CGEventType(rawValue: UInt32.max)!
+
+    func applicationDidFinishLaunching(notification: NSNotification) {
+        timerStart = NSDate()
+
+        updateButton()
+        NSTimer.scheduledTimer(buttonRefreshRate, userInfo: nil, repeats: true) { _ in self.updateButton() }
+    }
+
+    func updateButton() {
+        let sinceUserActivity = CGEventSourceSecondsSinceLastEventType(.CombinedSessionState, AnyInputEventType)
+        if (sinceUserActivity > userInactivityTimeout) {
+            timerStart = NSDate()
+            updateButton()
+        }
+
+        let duration = NSDate().timeIntervalSinceDate(timerStart)
+        let minutes = NSInteger(duration) / 60
+        statusItem.button.title = "\(minutes)m"
+    }
 }
--- Aware/StatusItem.swift
+++ Aware/StatusItem.swift
@@ -1,33 +1,20 @@
 import Cocoa
 
 class StatusItem: NSObject {
-    let item: NSStatusItem
+    private let item: NSStatusItem
 
-    @IBOutlet weak var menu: NSMenu!
+    @IBOutlet weak var menu: NSMenu! {
+        didSet {
+            item.menu = menu
+        }
+    }
+
+    var button: NSStatusBarButton {
+        return item.button!
+    }
 
     override init() {
         self.item = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
         super.init()
-    }
-
-    override func awakeFromNib() {
-        self.item.menu = menu
-
-        setDuration(NSTimeInterval())
-        UserActivityTimer(onUpdate: { duration in
-            self.setDuration(duration)
-        }).start()
-    }
-
-    func setDuration(duration: NSTimeInterval) {
-        if let button = self.item.button {
-            button.title = formatDuration(duration)
-        }
-    }
-
-    func formatDuration(duration: NSTimeInterval) -> String {
-        let seconds = NSInteger(duration)
-        let minutes = seconds / 60
-        return "\(minutes)m"
     }
 }
