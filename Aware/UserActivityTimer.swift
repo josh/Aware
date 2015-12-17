49afa1fc172af9ee4f420897718efbe8bbf5537e
Joshua Peek <josh@joshpeek.com>
2015-12-16 19:24:07 -0800

    Use CoreGraphics event idle timer
    Use CGEventSourceSecondsSinceLastEventType instead of installing
    global event handler.

--- Aware/UserActivityTimer.swift
+++ Aware/UserActivityTimer.swift
@@ -1,28 +1,24 @@
 import Cocoa
 
 class UserActivityTimer {
-    let eventMask: NSEventMask = [.KeyDownMask, .MouseMovedMask, .LeftMouseDownMask]
-
     var startTimestamp: NSDate
-    var userActivityTimestamp: NSDate
 
     var onUpdate: NSTimeInterval -> Void
 
     init(onUpdate: NSTimeInterval -> Void) {
-        self.userActivityTimestamp = NSDate()
         self.startTimestamp = NSDate()
         self.onUpdate = onUpdate
     }
 
-    func start() {
-        NSEvent.addGlobalMonitorForEventsMatchingMask(eventMask, interval: 30) { event in
-            self.userActivityTimestamp = NSDate()
-        }
+    // kCGAnyInputEventType isn't part of CGEventType enum
+    // defined in <CoreGraphics/CGEventTypes.h>
+    let AnyInputEventType = CGEventType(rawValue: UInt32.max)!
 
+    func start() {
         NSTimer.scheduledTimer(30, userInfo: nil, repeats: true) { _ in
             let now = NSDate()
 
-            let sinceUserActivity = now.timeIntervalSinceDate(self.userActivityTimestamp)
+            let sinceUserActivity = CGEventSourceSecondsSinceLastEventType(.CombinedSessionState, self.AnyInputEventType)
             if (NSInteger(sinceUserActivity) > 2 * 60) {
                 self.startTimestamp = now
             }
