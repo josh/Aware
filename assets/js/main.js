2455d763b77c7af56fe934745ecf04f6dbb162b0
Joshua Peek <josh@joshpeek.com>
2016-02-18 20:32:40 -0800

    Use what they tell me

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,12 +36,10 @@
 })();
 
 (function() {
-  window.GoogleAnalyticsObject = 'ga';
-  window.ga = function ga() {
-    ga.q.push(arguments);
-  };
-  ga.q = [];
-  ga.l = (new Date()).getTime();
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-74056549-2', 'auto');
   ga('send', 'pageview');
