f61a2daa333813db020f747d56d947530bf75e9c
Patrick Marsceill <pmarsceill@github.com>
2016-03-06 14:38:54 -0500

    Asset pngs for reference

---
+++ _site/assets/js/main.js
@@ -0,0 +1,36 @@
+(function() {
+  // Clicking menubar toggles "dark menu bar"
+  document.querySelector('.js-toggle-dark-menubar').addEventListener('click', function() {
+    this.classList.toggle('menubar-dark');
+  });
+
+  var timer = document.querySelector('.js-aware-timer');
+  var pageLoad = new Date() - (Math.random() * 1000 * 60 * 30);
+  function updateTimer() {
+    var now = new Date();
+    var min = Math.floor((now - pageLoad) / (1000 * 60));
+    var text = min + "m";
+
+    timer.textContent = text;
+  }
+
+  var clock = document.querySelector('.js-clock');
+  function updateClock() {
+    var now = new Date();
+    var day = now.toUTCString().split(",")[0];
+    var hour = (now.getHours() % 12);
+    var min = ("0" + now.getMinutes()).slice(-2);
+    var meridian = now.getHours() > 12 ? "PM" : "AM";
+    var text = day + " " + hour + ":" + min + " " + meridian;
+
+    clock.textContent = text;
+  }
+
+  setInterval(function() {
+    updateTimer();
+    updateClock();
+  }, 30000);
+
+  updateTimer();
+  updateClock();
+})()
