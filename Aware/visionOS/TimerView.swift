f1183c65535ba0673d0ba56fd4ff70709708524a
Joshua Peek <josh@joshpeek.com>
2024-02-19 20:23:54 -0800

    Extract main content view into view

---
+++ Aware/visionOS/TimerView.swift
@@ -0,0 +1,74 @@
+//
+//  TimerView.swift
+//  Aware
+//
+//  Created by Joshua Peek on 2/19/24.
+//
+
+#if os(visionOS)
+
+import SwiftUI
+
+struct TimerView: View {
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var protectedDataAvailablity = ProtectedDataAvailablity()
+    @State private var startDate: Date?
+
+    // TODO: Persist in scene storage
+    @State private var glassBackground: Bool = true
+
+    @State private var showSettings = false
+
+    var body: some View {
+        TimelineView(.everyMinute) { context in
+            let duration = context.date.timeIntervalSince(startDate ?? .now)
+            Text(duration, format: .custom)
+                .lineLimit(1)
+                .padding()
+                .font(.system(size: 900))
+                .minimumScaleFactor(0.01)
+                .glassBackgroundEffect(displayMode: self.glassBackground ? .always : .never)
+                .onTapGesture {
+                    showSettings.toggle()
+                }
+        }
+        .popover(isPresented: $showSettings) {
+            SettingsView(glassBackground: $glassBackground)
+                .frame(width: 400, height: 250)
+        }
+        .onChange(of: scenePhase, initial: true) { _, newValue in
+            switch newValue {
+            case .active, .inactive:
+                if self.startDate == nil {
+                    print("scenePhase change started timer")
+                    self.startDate = .now
+                }
+            case .background:
+                ()
+            default:
+                ()
+            }
+        }
+        .onChange(of: protectedDataAvailablity.isAvailable) { oldValue, newValue in
+            if oldValue == false && newValue == true {
+                if self.startDate == nil {
+                    print("protectedDataAvailablity change started timer")
+                    self.startDate = .now
+                }
+            } else if oldValue == true && newValue == false {
+                print("protectedDataAvailablity stopped timer")
+                self.startDate = nil
+            }
+        }
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 200, height: 100)) {
+    TimerView()
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    TimerView()
+}
+
+#endif
--- Aware/visionOS/VisionScene.swift
+++ Aware/visionOS/VisionScene.swift
@@ -10,61 +10,13 @@
 import SwiftUI
 
 struct VisionScene: Scene {
-    @Environment(\.scenePhase) private var scenePhase
-    @State private var protectedDataAvailablity = ProtectedDataAvailablity()
-    @State private var startDate: Date?
-
-    // TODO: Persist in scene storage
-    @State private var glassBackground: Bool = true
-
-    @State private var showSettings = false
-
     var body: some Scene {
         WindowGroup {
-            TimelineView(.everyMinute) { context in
-                let duration = context.date.timeIntervalSince(startDate ?? .now)
-                Text(duration, format: .custom)
-                    .lineLimit(1)
-                    .padding()
-                    .font(.system(size: 900))
-                    .minimumScaleFactor(0.01)
-                    .glassBackgroundEffect(displayMode: self.glassBackground ? .always : .never)
-                    .onTapGesture {
-                        showSettings.toggle()
-                    }
-            }
-            .popover(isPresented: $showSettings) {
-                SettingsView(glassBackground: $glassBackground)
-                    .frame(width: 400, height: 250)
-            }
+            TimerView()
         }
         .defaultSize(CGSize(width: 200, height: 100))
         .windowResizability(.contentMinSize)
         .windowStyle(.plain)
-        .onChange(of: scenePhase, initial: true) { _, newValue in
-            switch newValue {
-            case .active, .inactive:
-                if self.startDate == nil {
-                    print("scenePhase change started timer")
-                    self.startDate = .now
-                }
-            case .background:
-                ()
-            default:
-                ()
-            }
-        }
-        .onChange(of: protectedDataAvailablity.isAvailable) { oldValue, newValue in
-            if oldValue == false && newValue == true {
-                if self.startDate == nil {
-                    print("protectedDataAvailablity change started timer")
-                    self.startDate = .now
-                }
-            } else if oldValue == true && newValue == false {
-                print("protectedDataAvailablity stopped timer")
-                self.startDate = nil
-            }
-        }
     }
 }
 
