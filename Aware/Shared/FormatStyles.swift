c308e5c71012b0ab2ae4441e9b4950f533565d83
Joshua Peek <josh@joshpeek.com>
2024-03-09 15:49:54 -0800

    Add formatter support to Duration type

--- Aware/Shared/FormatStyles.swift
+++ Aware/Shared/FormatStyles.swift
@@ -17,12 +17,39 @@ struct TimeIntervalFormatStyle<S>: FormatStyle where S: FormatStyle, S.FormatInp
     }
 
     func format(_ value: TimeInterval) -> S.FormatOutput {
-        let range = Date.now ..< Date.now.addingTimeInterval(value)
+        let referenceDate = Date(timeIntervalSinceReferenceDate: 0)
+        let range = referenceDate ..< Date(timeIntervalSinceReferenceDate: value)
+        return range.formatted(style)
+    }
+}
+
+/// Wrapper to provide any `Range<Date>` formatter to `Duration`.
+struct DurationFormatStyle<S>: FormatStyle where S: FormatStyle, S.FormatInput == Range<Date> {
+    let style: S
+
+    init(_ style: S) {
+        self.style = style
+    }
+
+    func format(_ value: Duration) -> S.FormatOutput {
+        let referenceDate = Date(timeIntervalSinceReferenceDate: 0)
+        let timeInterval = TimeInterval(value.components.seconds)
+        let range = referenceDate ..< Date(timeIntervalSinceReferenceDate: timeInterval)
         return range.formatted(style)
     }
 }
 
 extension FormatStyle where Self == TimeIntervalFormatStyle<Date.ComponentsFormatStyle> {
+    static func components(style: Date.ComponentsFormatStyle.Style, fields: Set<Date.ComponentsFormatStyle.Field>? = nil) -> Self {
+        .init(Date.ComponentsFormatStyle.components(style: style, fields: fields))
+    }
+
+    static var timeDuration: Self {
+        .init(Date.ComponentsFormatStyle.timeDuration)
+    }
+}
+
+extension FormatStyle where Self == DurationFormatStyle<Date.ComponentsFormatStyle> {
     static func components(style: Date.ComponentsFormatStyle.Style, fields: Set<Date.ComponentsFormatStyle.Field>? = nil) -> Self {
         .init(Date.ComponentsFormatStyle.components(style: style, fields: fields))
     }
@@ -40,14 +67,27 @@ struct AbbreviatedTimeIntervalFormatStyle: FormatStyle {
     /// - Parameter value: The time interval to format.
     /// - Returns: A string representation of the time interval.
     func format(_ value: TimeInterval) -> String {
-        if bounds.contains(value) {
-            return style.format(value)
-        } else {
-            return style.format(0.0)
-        }
+        guard bounds.contains(value) else { return style.format(0.0) }
+        return style.format(value)
+    }
+}
+
+struct AbbreviatedDurationFormatStyle: FormatStyle {
+    private var style: DurationFormatStyle<Date.ComponentsFormatStyle> = .components(style: .condensedAbbreviated, fields: [.hour, .minute])
+
+    /// Formats a duration as a human readable string.
+    /// - Parameter value: The duration to format.
+    /// - Returns: A string representation of the duration.
+    func format(_ value: Duration) -> String {
+        guard value > .zero else { return style.format(Duration.zero) }
+        return style.format(value)
     }
 }
 
 extension FormatStyle where Self == AbbreviatedTimeIntervalFormatStyle {
     static var abbreviatedTimeInterval: Self { .init() }
+}
+
+extension FormatStyle where Self == AbbreviatedDurationFormatStyle {
+    static var abbreviatedDuration: Self { .init() }
 }
--- AwareTests/FormatStylesTests.swift
+++ AwareTests/FormatStylesTests.swift
@@ -3,13 +3,15 @@ import XCTest
 @testable import Aware
 
 class FormatStyleTests: XCTestCase {
-    func testFormatted() {
+    func testTimeIntervalFormatted() {
+        XCTAssertEqual(TimeInterval(15).formatted(.timeDuration), "15")
         XCTAssertEqual(TimeInterval(60).formatted(.timeDuration), "1:00")
+        XCTAssertEqual(TimeInterval(15).formatted(.components(style: .spellOut)), "fifteen seconds")
         XCTAssertEqual(TimeInterval(60).formatted(.components(style: .spellOut)), "one minute")
         XCTAssertEqual(TimeInterval(900).formatted(.abbreviatedTimeInterval), "15m")
     }
 
-    func testAbbreviatedTimeIntervalFormatStyle() {
+    func testTimeIntervalAbbreviatedTimeIntervalFormatStyle() {
         let formatter = AbbreviatedTimeIntervalFormatStyle()
 
         XCTAssertEqual(formatter.format(0), "0m")
@@ -48,5 +50,51 @@ class FormatStyleTests: XCTestCase {
         XCTAssertEqual(formatter.format(Double.greatestFiniteMagnitude), "0m")
         XCTAssertEqual(formatter.format(Double.leastNormalMagnitude), "0m")
         XCTAssertEqual(formatter.format(Double.leastNonzeroMagnitude), "0m")
+    }
+
+    func testDurationFormatted() {
+        XCTAssertEqual(Duration.seconds(15).formatted(.time(pattern: .hourMinuteSecond)), "0:00:15")
+        XCTAssertEqual(Duration.seconds(15).formatted(.timeDuration), "15")
+        XCTAssertEqual(Duration.seconds(60).formatted(.timeDuration), "1:00")
+        XCTAssertEqual(Duration.seconds(15).formatted(.components(style: .spellOut)), "fifteen seconds")
+        XCTAssertEqual(Duration.seconds(60).formatted(.components(style: .spellOut)), "one minute")
+        XCTAssertEqual(Duration.seconds(900).formatted(.abbreviatedDuration), "15m")
+    }
+
+    func testDurationAbbreviatedTimeIntervalFormatStyle() {
+        let formatter = AbbreviatedDurationFormatStyle()
+
+        XCTAssertEqual(formatter.format(.seconds(0)), "0m")
+        XCTAssertEqual(formatter.format(.seconds(1)), "0m")
+        XCTAssertEqual(formatter.format(.seconds(30)), "0m")
+        XCTAssertEqual(formatter.format(.seconds(59)), "0m")
+
+        XCTAssertEqual(formatter.format(.seconds(60)), "1m")
+        XCTAssertEqual(formatter.format(.seconds(61)), "1m")
+        XCTAssertEqual(formatter.format(.seconds(119)), "1m")
+
+        XCTAssertEqual(formatter.format(.seconds(120)), "2m")
+        XCTAssertEqual(formatter.format(.seconds(300)), "5m")
+        XCTAssertEqual(formatter.format(.seconds(900)), "15m")
+        XCTAssertEqual(formatter.format(.seconds(1800)), "30m")
+        XCTAssertEqual(formatter.format(.seconds(2700)), "45m")
+        XCTAssertEqual(formatter.format(.seconds(3540)), "59m")
+        XCTAssertEqual(formatter.format(.seconds(3599)), "59m")
+
+        XCTAssertEqual(formatter.format(.seconds(3600)), "1h")
+        XCTAssertEqual(formatter.format(.seconds(3601)), "1h")
+        XCTAssertEqual(formatter.format(.seconds(3660)), "1h 1m")
+        XCTAssertEqual(formatter.format(.seconds(4500)), "1h 15m")
+        XCTAssertEqual(formatter.format(.seconds(5400)), "1h 30m")
+        XCTAssertEqual(formatter.format(.seconds(6300)), "1h 45m")
+
+        XCTAssertEqual(formatter.format(.seconds(7200)), "2h")
+
+        XCTAssertEqual(formatter.format(.seconds(-1)), "0m")
+        XCTAssertEqual(formatter.format(.seconds(-90)), "0m")
+        XCTAssertEqual(formatter.format(.seconds(-3600)), "0m")
+
+        XCTAssertEqual(formatter.format(.seconds(Int.max)), "0m")
+        XCTAssertEqual(formatter.format(.seconds(Int.min)), "0m")
     }
 }
