4fede2e39635133825f0e3edd72e3ec1b2c0f522
Joshua Peek <josh@joshpeek.com>
2024-03-16 17:00:02 -0700

    Make NSEvent.Events sendable

--- Aware/macOS/NSEvent+AsyncStream.swift
+++ Aware/macOS/NSEvent+AsyncStream.swift
@@ -10,28 +10,63 @@
 import AppKit
 
 extension NSEvent {
-    static func globalEvents(matching mask: NSEvent.EventTypeMask) -> AsyncStream<NSEvent> {
-        AsyncStream { continuation in
+    struct Monitor: @unchecked Sendable {
+        /// The event handler object
+        let eventMonitor: Any?
+
+        fileprivate init(_ eventMonitor: Any?) {
+            self.eventMonitor = eventMonitor
+        }
+
+        /// Removes the specified event monitor.
+        func cancel() {
+            assert(eventMonitor != nil, "event monitor failed to install")
+            if let eventMonitor {
+                NSEvent.removeMonitor(eventMonitor)
+            }
+        }
+    }
+
+    final class Events: AsyncSequence, @unchecked Sendable {
+        typealias AsyncIterator = Iterator
+        typealias Element = NSEvent
+
+        struct Iterator: AsyncIteratorProtocol {
+            private var iterator: AsyncStream<NSEvent>.Iterator
+
+            fileprivate init(iterator: AsyncStream<NSEvent>.Iterator) {
+                self.iterator = iterator
+            }
+
+            mutating func next() async -> NSEvent? {
+                await iterator.next()
+            }
+        }
+
+        private let stream: AsyncStream<NSEvent>
+
+        fileprivate init(matching mask: NSEvent.EventTypeMask, bufferingPolicy: AsyncStream<NSEvent>.Continuation.BufferingPolicy = .unbounded) {
+            let (stream, continuation) = AsyncStream.makeStream(of: NSEvent.self, bufferingPolicy: bufferingPolicy)
+            self.stream = stream
+
             let monitor = NSEvent.addGlobalMonitorForEvents(matching: mask) { event in
                 continuation.yield(event)
             }
 
-            assert(monitor != nil, "failed to add monitor")
-            guard let monitor else {
-                continuation.finish()
-                return
-            }
-
-            let sendableMonitor = UncheckedSendable(value: monitor)
-            continuation.onTermination = { _ in
-                NSEvent.removeMonitor(sendableMonitor.value)
+            let eventMonitor = Monitor(monitor)
+            continuation.onTermination = { [eventMonitor] _ in
+                eventMonitor.cancel()
             }
         }
-    }
-}
 
-private struct UncheckedSendable: @unchecked Sendable {
-    var value: Any
+        func makeAsyncIterator() -> Iterator {
+            Iterator(iterator: stream.makeAsyncIterator())
+        }
+    }
+
+    static func globalEvents(matching mask: NSEvent.EventTypeMask) -> NSEvent.Events {
+        NSEvent.Events(matching: mask)
+    }
 }
 
 #endif
