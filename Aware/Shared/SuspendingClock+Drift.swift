d0ee6423d902fe13651787e15af87c74f68a6de0
Joshua Peek <josh@joshpeek.com>
2024-03-24 13:04:33 -0700

    Add start/finish log statements

--- Aware/Shared/SuspendingClock+Drift.swift
+++ Aware/Shared/SuspendingClock+Drift.swift
@@ -45,6 +45,9 @@ extension SuspendingClock {
     /// - Returns: The drift duration above the `threshold`
     @discardableResult
     func monitorDrift(threshold: Duration) async throws -> Duration {
+        logger.debug("Starting SuspendingClock drift monitor")
+        defer { logger.debug("Finished SuspendingClock drift monitor") }
+
         let continuousClock = ContinuousClock()
         let suspendingClock = self
 
