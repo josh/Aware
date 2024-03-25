90894a7fcacab6be84989d489b95485dd94e3d60
Joshua Peek <josh@joshpeek.com>
2024-03-24 19:05:40 -0700

    Add a ton of logging to bg tasks, who knows if its working

--- Aware/Shared/BackgroundTask.swift
+++ Aware/Shared/BackgroundTask.swift
@@ -14,12 +14,30 @@ private nonisolated(unsafe) let logger = Logger(
 )
 
 @available(macOS, unavailable)
-struct BackgroundTask {
+actor BackgroundTask {
+    enum TaskRequestType {
+        case appRefresh
+        case processing(requiresExternalPower: Bool, requiresNetworkConnectivity: Bool)
+    }
+
+    struct ScheduledTask {
+        let submittedAt: Date
+        let earliestBeginAt: Date
+    }
+
+    struct RanTask {
+        let task: ScheduledTask?
+        let ranAt: Date
+    }
+
     let identifier: String
     let notification: Notification.Name
     private let taskRequestType: TaskRequestType
 
-    static func appRefresh(_ identifier: String) -> Self {
+    var scheduledTask: ScheduledTask?
+    var lastRanTask: RanTask?
+
+    static func appRefresh(_ identifier: String) -> BackgroundTask {
         BackgroundTask(identifier: identifier, taskRequestType: .appRefresh)
     }
 
@@ -27,7 +45,7 @@ struct BackgroundTask {
         _ identifier: String,
         requiresExternalPower: Bool = false,
         requiresNetworkConnectivity: Bool = false
-    ) -> Self {
+    ) -> BackgroundTask {
         BackgroundTask(
             identifier: identifier,
             taskRequestType: .processing(
@@ -37,19 +55,32 @@ struct BackgroundTask {
         )
     }
 
-    enum TaskRequestType {
-        case appRefresh
-        case processing(requiresExternalPower: Bool, requiresNetworkConnectivity: Bool)
-    }
-
     init(identifier: String, taskRequestType: TaskRequestType) {
         self.identifier = identifier
         self.taskRequestType = taskRequestType
         notification = Notification.Name(identifier)
     }
 
-    func cancel() {
-        BGTaskScheduler.shared.cancel(taskRequestWithIdentifier: identifier)
+    nonisolated func schedule(for beginDate: Date) {
+        Task { await self._schedule(for: beginDate) }
+    }
+
+    nonisolated func reschedule(for beginDate: Date) {
+        Task { await self._reschedule(for: beginDate) }
+    }
+
+    nonisolated func schedule(after duration: Duration) {
+        let instant = UTCClock.Instant.now.advanced(by: duration)
+        schedule(for: instant.date)
+    }
+
+    nonisolated func reschedule(after duration: Duration) {
+        let instant = UTCClock.Instant.now.advanced(by: duration)
+        reschedule(for: instant.date)
+    }
+
+    nonisolated func cancel() {
+        Task { await self._cancel() }
     }
 
     private var request: BGTaskRequest {
@@ -64,27 +95,65 @@ struct BackgroundTask {
         }
     }
 
-    func reschedule(for beginDate: Date) {
-        cancel()
-        schedule(for: beginDate)
+    fileprivate func run() {
+        let identifier = self.identifier
+        logger.log("Starting background task: \(identifier, privacy: .public)")
+
+        if let scheduledTask {
+            let submittedAgo: Duration = UTCClock.Instant(scheduledTask.submittedAt).duration(to: .now)
+            let earliestBeginAgo: Duration = UTCClock.Instant(scheduledTask.earliestBeginAt).duration(
+                to: .now)
+            logger.log("Background submitted at \(scheduledTask.submittedAt), \(submittedAgo) ago")
+            logger.log(
+                "Requested to run after \(scheduledTask.earliestBeginAt), \(earliestBeginAgo) after")
+        } else {
+            logger.error(
+                "Running background task, but no scheduled \(identifier, privacy: .public) task noted")
+        }
+        lastRanTask = RanTask(task: scheduledTask, ranAt: .now)
+
+        let notification = Notification(name: self.notification, object: self)
+        logger.log("Posting \(notification.name.rawValue) notification")
+        NotificationCenter.default.post(notification)
+
+        logger.log("Finished background task: \(identifier, privacy: .public)")
     }
 
-    func reschedule(after duration: Duration) {
-        cancel()
-        schedule(after: duration)
+    private func _cancel() async {
+        let pendingCount = await countPendingTaskRequests()
+        guard pendingCount > 0 else {
+            logger.debug("No scheduled tasks to cancel")
+            return
+        }
+        assert(pendingCount <= 1, "more than one background task was scheduled")
+
+        scheduledTask = nil
+        let identifier = identifier
+        logger.info("Canceling \(pendingCount) \(identifier, privacy: .public) task")
+        BGTaskScheduler.shared.cancel(taskRequestWithIdentifier: identifier)
     }
 
-    func schedule(after duration: Duration) {
-        let instant = UTCClock.Instant.now.advanced(by: duration)
-        schedule(for: instant.date)
+    private func _reschedule(for beginDate: Date) async {
+        let earliestBeginAt: Date = await earliestPendingTaskRequestBeginDate() ?? .distantPast
+        guard beginDate > earliestBeginAt.addingTimeInterval(60) else {
+            let identifier = identifier
+            logger.debug("\(identifier, privacy: .public) task already scheduled for \(beginDate)")
+            return
+        }
+
+        await _cancel()
+        _schedule(for: beginDate)
     }
 
-    func schedule(for beginDate: Date) {
+    private func _schedule(for beginDate: Date) {
+        let identifier = identifier
         let request = request
+
         request.earliestBeginDate = beginDate
 
         do {
             try BGTaskScheduler.shared.submit(request)
+            scheduledTask = ScheduledTask(submittedAt: .now, earliestBeginAt: beginDate)
             logger.info(
                 "Scheduled \(identifier, privacy: .public) task after \(beginDate, privacy: .public)")
         } catch let error as BGTaskScheduler.Error {
@@ -104,16 +173,23 @@ struct BackgroundTask {
             logger.error("Unknown error scheduling \(identifier, privacy: .public) task: \(error)")
         }
     }
+
+    private nonisolated func countPendingTaskRequests() async -> Int {
+        await BGTaskScheduler.shared.pendingTaskRequests().filter { request in
+            request.identifier == self.identifier
+        }.count
+    }
+
+    private nonisolated func earliestPendingTaskRequestBeginDate() async -> Date? {
+        await BGTaskScheduler.shared.pendingTaskRequests().compactMap(\.earliestBeginDate).min()
+    }
 }
 
 @available(macOS, unavailable)
 extension Scene {
     func backgroundTask(_ task: BackgroundTask) -> some Scene {
         backgroundTask(.appRefresh(task.identifier)) {
-            logger.log("Starting background task: \(task.identifier, privacy: .public)")
-            let notification = Notification(name: task.notification, object: BGTaskScheduler.shared)
-            NotificationCenter.default.post(notification)
-            logger.log("Finished background task: \(task.identifier, privacy: .public)")
+            await task.run()
         }
     }
 }
